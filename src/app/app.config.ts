47b1c02928477c2c29cb7d2b61b67764e392ab2b
Hugo Cano Soriano <hugojulio.canosoriano@soprasteria.com>
2025-10-10 18:40:38 +0200

    Loading behaviour

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,18 @@
 import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient } from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import { provideTransloco } from '@jsverse/transloco';
 import { routes } from './app.routes';
 import { TranslocoHttpLoader } from './transloco-loader';
+import {loadingInterceptor} from './interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideHttpClient(), provideTransloco({
-        config: { 
+    provideRouter(routes), provideHttpClient(withInterceptors([loadingInterceptor])), provideTransloco({
+        config: {
           availableLangs: ['en','es','fr','de'],
           defaultLang: 'en',
           reRenderOnLangChange: true,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,48 +1,52 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, inject, OnInit} from '@angular/core';
 import { RestApiService } from '../../services/rest-api.service';
+import {NgClass} from '@angular/common';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'] 
+  imports: [NgClass],
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
   public restApiService = inject(RestApiService)
-  
-  // Variable para almacenar la clave de API introducida por el usuario
   public apiKey: string = '';
-
+  public isLoading: boolean = false;
+  private readonly cdr = inject(ChangeDetectorRef);
 
   public ngOnInit(): void {
     // Lógica de inicialización, como comprobar si ya hay una API key almacenada
   }
 
   public submitApiKey(): void {
-
     if (this.apiKey.length > 10 ) {
+      this.isLoading = true;
       console.debug('ApiKey:', this.apiKey);
+      this.restApiService.tokeninfo(this.apiKey)
+        .pipe(finalize(() => {
+          this.isLoading = false;
+          this.cdr.detectChanges();}))
+        .subscribe({
+          next: json => {
+            console.debug('ApiKey is Ok!');
+            console.debug('Apikey=' + json.api_key);
+            console.debug('permissions=' + json.permissions);
+          },
+          error: err => {
+            console.error(err);
+          }
+        })
 
-      this.restApiService.tokeninfo(this.apiKey).subscribe({
-        next: json => {
-          console.debug('ApiKey is Ok!');
-          console.debug('Apikey=' + json.api_key);
-          console.debug('permissions=' + json.permissions);
-        },
-        error: err => {
-          console.error(err);
-        }  
-      })
-
-      
 
       // Lógica real:
       // 1. Llamar a un servicio (ej. accountService.validateAndSave(this.apiKey)).
       // 2. Navegar a la página de "Account" o "Characters".
 
       // Ejemplo de redirección (requiere Router):
-      // this.router.navigate(['/account']); 
+      // this.router.navigate(['/account']);
 
     } else {
       alert('Por favor, introduce una clave de API válida de Guild Wars 2.');
