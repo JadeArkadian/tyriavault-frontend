014dcda04c2d261df389d748608b9cf79b631b5e
Jade Arkadian <hj.cano@alumno.ucjc.edu>
2025-10-13 23:08:53 +0200

    UT fix

--- src/app/pages/home/home.spec.ts
+++ src/app/pages/home/home.spec.ts
@@ -1,13 +1,19 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+
 
 
 describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
-      providers: [provideZonelessChangeDetection()]
+      providers: [
+        provideZonelessChangeDetection(),    
+        provideHttpClient(), 
+        provideHttpClientTesting() ]
     }).compileComponents();
   });
 
