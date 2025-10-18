04220896150d8dff13312a73db85320f6aa7a218
Jade Arkadian <viento2k6@gmail.com>
2025-10-19 01:15:36 +0200

    Navigations changes

--- src/app/pages/gameaccount/gameaccount.component.ts
+++ src/app/pages/gameaccount/gameaccount.component.ts
@@ -1,10 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gameaccount',
   imports: [],
+  standalone: true,
   templateUrl: './gameaccount.component.html',
   styleUrl: './gameaccount.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GameaccountComponent { }
+export class GameaccountComponent implements OnInit {
+
+  
+  ngOnInit(): void {
+    console.log('GameaccountComponent initialized');
+  }
+
+}
