ecaf1812dd027dfc5b096d909142d70ec8cd105a
Jade Arkadian <viento2k6@gmail.com>
2025-10-20 00:43:16 +0200

    Code cleanup and some documentation

--- src/app/services/rest-api.service.ts
+++ src/app/services/rest-api.service.ts
@@ -4,6 +4,9 @@ import { map, Observable } from 'rxjs';
 import { environment } from './../../environments/environment'
 import { AccountInfoResponse, TokenInfoResponse } from '../interfaces/api-responses';
 
+/**
+ * The Rest API Service. Its a WS client used to perform the calls to the backend part of the app
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -12,11 +15,21 @@ export class RestApiService {
   private readonly apiURL = environment.apiUrl;
   private readonly httpClient = inject(HttpClient);
 
+  /**
+   * Call to the tokeninfo service
+   * @param apiKey A valid apiKey
+   * @returns
+   */
   public tokeninfo(apiKey:string): Observable<TokenInfoResponse> {
     const headers = this.buildHeaders(apiKey);
     return this.httpClient.get<TokenInfoResponse>(`${this.apiURL}/api/v1/common/tokeninfo`, { headers });
   }
 
+  /**
+   * Call to the account service. Must have
+   * @param apiKey 
+   * @returns 
+   */
   public accountInfo(apiKey:string): Observable<AccountInfoResponse> {
     const headers = this.buildHeaders(apiKey);
     return this.httpClient.get<AccountInfoResponse>(`${this.apiURL}/api/v1/account`, { headers });
