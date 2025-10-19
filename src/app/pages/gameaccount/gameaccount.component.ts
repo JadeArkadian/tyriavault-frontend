df5eb71ceae9052aa645ce2f08ca3504faff30dc
Jade Arkadian <viento2k6@gmail.com>
2025-10-19 03:26:04 +0200

    Account info retrieval WIP

--- src/app/pages/gameaccount/gameaccount.component.ts
+++ src/app/pages/gameaccount/gameaccount.component.ts
@@ -1,8 +1,13 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { RestApiService } from '../../services/rest-api.service';
+import { StorageService } from '../../services/storage.service';
+import { finalize } from 'rxjs';
+import { AccountInfoResponse } from '../../interfaces/api-responses';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-gameaccount',
-  imports: [],
+  imports: [DatePipe],
   standalone: true,
   templateUrl: './gameaccount.component.html',
   styleUrl: './gameaccount.component.css',
@@ -10,9 +15,46 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 })
 export class GameaccountComponent implements OnInit {
 
-  
-  ngOnInit(): void {
-    console.log('GameaccountComponent initialized');
+  //- Injects
+  private readonly restApiService = inject(RestApiService);
+  private readonly cdr = inject(ChangeDetectorRef);
+  private readonly storageService = inject(StorageService);
+
+  //- Properties
+  public isLoading = false;
+  public accountData!: AccountInfoResponse;
+
+  /**
+   * On Init event
+   */
+  public ngOnInit(): void {
+    console.debug('GameaccountComponent initialized');
+    const apiKey = this.storageService.apiKey()
+    
+    // Got the apikey? Ok, lets call the backend service
+    this.getAccountInfo(apiKey);
+
+  }
+
+  private getAccountInfo(apiKey: string | undefined): void {
+    // Add a loading check just to avoid machine gun like calls
+    if (apiKey && !this.isLoading) {
+      this.isLoading = true;
+      this.restApiService.accountInfo(apiKey)
+      .pipe(finalize(() => {
+          this.isLoading = false;
+          this.cdr.detectChanges();
+        }))
+      .subscribe({
+        next: (response) => {
+          this.accountData = response;
+          console.debug(response)
+        },
+        error: (err) => {
+          console.debug(err)
+        }
+      })
+    }
   }
 
 }
